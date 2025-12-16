cfc72124ded03855833729074986d6cffbc4999b
Samruddhi Patil <your_email@example.com>
2025-12-16 22:16:54 +0530

    security config completely added

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -8,6 +8,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +32,11 @@ public class SecurityConfig {
     // Authentication provider (uses UserDetailsService + PasswordEncoder)
     private final AuthenticationProvider authenticationProvider;
 
+        private final UserDetailsService userDetailsService;
+
+
+
+    
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
@@ -47,4 +63,6 @@ public class SecurityConfig {
 
         return http.build();
     }
+    
+    
 }
